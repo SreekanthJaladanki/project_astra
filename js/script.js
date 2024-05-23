c7b48693572b0f8414437ac8b039542fc3b544c4
Deeksha <Deeksha.s@qspiders.in>
2024-05-23 18:59:35 +0530

    Modified  files

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // Function to update time and date
 function updateTimeAndDate() {
     const datetimeElement = document.getElementById('datetime');
@@ -11,3 +12,21 @@ function updateTimeAndDate() {
 
 // Update time and date every second
 setInterval(updateTimeAndDate, 1000);
+
+// Preloader logic for initial page load
+window.addEventListener('load', function() {
+    const preloader = document.getElementById('preloader');
+    setTimeout(function() {
+        preloader.style.display = 'none';
+    }, 2000); // Display preloader for 2 seconds
+});
+
+// Preloader logic for card clicks
+function showCardPreloader(page) {
+    const preloader = document.getElementById('preloader');
+    preloader.style.display = 'flex';
+
+    setTimeout(function() {
+        window.location.href = page;
+    }, 2000); // Display preloader for 2 seconds before redirecting
+}
