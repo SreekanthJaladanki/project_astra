77079f8480630bb0c4ce11e0acca64bd90d59c9a
arati <abc@gmail.com>
2024-05-25 11:51:13 +0530

    Modified dashboard

---
+++ Admin/dash1.js
@@ -0,0 +1,45 @@
+const chartData = {
+    labels: ["emp1", "emp2", "emp3", "Others"],
+    data: [10, 27, 10, 43],
+  };
+  
+  const myChart = document.querySelector(".my-chart");
+  const ul = document.querySelector(".programming-stats .details ul");
+  
+  new Chart(myChart, {
+    type: "doughnut",
+    data: {
+      labels: chartData.labels,
+      datasets: [
+        {
+          label: "Count",
+          data: chartData.data,
+        },
+      ],
+    },
+    options: {
+      borderWidth: 10,
+      borderRadius: 2,
+      hoverBorderWidth: 0,
+      plugins: {
+        legend: {
+          display: false,
+        },
+      },
+    },
+  });
+  
+  const populateUl = () => {
+    chartData.labels.forEach((l, i) => {
+      let li = document.createElement("li");
+      li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+      ul.appendChild(li);
+    });
+  };
+  
+  populateUl();
+
+
+
+
+  
