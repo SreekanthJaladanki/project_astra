364b4bae05ac21ae2f6b6d3346b81faf617c2507
Sreekanth <sreekanthjaladanki@gmail.com>
2024-06-25 17:19:45 +0530

    Modified countdown

--- js/countdown.js
+++ js/countdown.js
@@ -43,7 +43,7 @@
         }
 
         window.onload = function () {
-            var deadline = new Date("June 30, 2024 10:00:00");
+            var deadline = new Date("July 1, 2024 10:00:00");
             startTimer("clock", deadline);
         };
    
